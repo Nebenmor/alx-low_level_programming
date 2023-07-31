c5dd54f930b2a0b75c81e95f9f6b9469ab051bef
Nebenmor <nebenmor.anthony@gmail.com>
2023-07-31 13:18:33 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -6,13 +6,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 char *s = "hello, world";
 char *f = "world";
 char *t;
 
-t = _strpbrk(s, f);
+t = _strstr(s, f);
 printf("%s\n", t);
 return (0);
 }
