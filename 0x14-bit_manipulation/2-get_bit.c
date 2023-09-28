66da2b1ac2e0f2437c4afe19495cb9e954741b88
Nebenmor <nebenmor.anthony@gmail.com>
2023-09-28 05:30:28 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,13 +5,12 @@
  * @n: number to search
  * @index: index of the bit (0-based)
  *
- * Return: value of the bit at the specified index, or -1 if the index is invalid
+ * Return: value of index bit, or -1 if the index is invalid
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index > 63) /* Ensure index is within the valid range for a 64-bit number */
+if (index > 63) /* Keeps index in valid range for a 64-bit number */
 return (-1);
 
 return ((n >> index) & 1);
 }
-
